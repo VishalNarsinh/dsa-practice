c13b3771f55019313ddafff4fe05520d607711b4
Vishal <vishalnarsinh@gmail.com>
2024-05-23 18:58:59 +0530

    Basic Pattern programs without spaces

--- src/com/dsa/basic/pattern/Pattern2.java
+++ src/com/dsa/basic/pattern/Pattern2.java
@@ -1,10 +1,10 @@
 package com.dsa.basic.pattern;
 
-public class Pattern1 {
+public class Pattern2 {
     public static void main(String[] args) {
         int n = 5;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
                 System.out.print("*");
             }
             System.out.println();
