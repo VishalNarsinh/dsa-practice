3d679a363894ab272f4346185da264c4d1c7d436
Vishal <vishalnarsinh@gmail.com>
2024-05-23 19:20:30 +0530

    Fixed Pattern 5 & 6

--- src/com/dsa/basic/pattern/Pattern5.java
+++ src/com/dsa/basic/pattern/Pattern5.java
@@ -2,10 +2,10 @@ package com.dsa.basic.pattern;
 
 public class Pattern5 {
     public static void main(String[] args) {
-        int n = 5;
-        for (int i = n; i >=1 ; i--) {
-            for (int j = 1; j <= i; j++) {
-                System.out.print("*");
+        int n=5;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j < n-i+1; j++) {
+                System.out.print("* ");
             }
             System.out.println();
         }
